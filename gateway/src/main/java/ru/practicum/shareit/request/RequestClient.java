3fe5c14c650ef69d2f3d19311c929d0cac230e6f
Dinar Khazipov <khazipovdn@yandex.ru>
2025-03-24 23:09:55 +0300

    Добавление ветки add-item-requests-and-gateway-ver2.4

--- gateway/src/main/java/ru/practicum/shareit/request/RequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestClient.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
-import ru.practicum.shareit.request.dto.ItemRequestResponseDto;
 
 import java.util.Map;
 
